6543ebd4cbe7472098a38b51cba24ed08c959c53
corycaprioli <cory.caprioli@csu.fullerton.edu>
2023-02-26 03:20:12 -0800

    changed

--- main.cpp
+++ main.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "input.txt"
+//#include "output.txt"
 
+//void lexer() {}
+/* Keyword, Seperator, Identifier, Operator, Real, Integer */
+
+int main() {
+
+  /*std::vector<std::string> keywords{"if", "for", "while", "do", "int", "double", "float", "bool", };
+  std::vector<std::string> operators{"+", "-", "*", "/", "++", "%", "--", "=", "+=", "-=", "*=", "/=", "%=", "==", "!=", ">", "<", ">=", "<=", "&&", "||", "!", "&", "|", "^", "~", "<<", ">>"};
+  std::vector<std::string> seperators{"(", ")", ";", "{", "}", };
+  
+  int token;*/
+
+  std::fstream fs("input.txt"); 
+    char c; 
+    while (fs >> c) 
+    { 
+        std::cout << c; 
+    }  
+
+  return 0;
+}

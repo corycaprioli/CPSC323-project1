b371746c21a06a92bc1ea0e73023182543d536f8
corycaprioli <cory.caprioli@csu.fullerton.edu>
2023-03-03 21:29:29 -0800

    finished

--- main.cpp
+++ main.cpp
@@ -1,27 +1,51 @@
 #include <iostream>
-#include <fstream>
+#include <iomanip>
 #include <string>
-#include <vector>
-#include "input.txt"
-//#include "output.txt"
-
-//void lexer() {}
-/* Keyword, Seperator, Identifier, Operator, Real, Integer */
+#include <fstream>
+#include "lexer.h"
 
 int main() {
+  std::string filename;
+	std::string line;
 
-  /*std::vector<std::string> keywords{"if", "for", "while", "do", "int", "double", "float", "bool", };
-  std::vector<std::string> operators{"+", "-", "*", "/", "++", "%", "--", "=", "+=", "-=", "*=", "/=", "%=", "==", "!=", ">", "<", ">=", "<=", "&&", "||", "!", "&", "|", "^", "~", "<<", ">>"};
-  std::vector<std::string> seperators{"(", ")", ";", "{", "}", };
-  
-  int token;*/
+  std::ifstream in_file;
+	std::ofstream out_file("output.txt");
 
-  std::fstream fs("input.txt"); 
-    char c; 
-    while (fs >> c) 
-    { 
-        std::cout << c; 
-    }  
+	do {
+    //ask for filename
+		std::cout << "Enter an input file name or \"stop\" to exit program: ";
+		std::cin >> filename;
 
+    if (filename != "stop") {
+      in_file.open(filename); //open file
+
+      //check if file is open
+      if (!in_file.is_open()) {
+        throw std::invalid_argument("file does not exist");
+        return 0;
+      }
+
+			out_file << "lexer output:\n";
+			out_file << std::left << std::setw(20) << "Token" << std::setw(20) << "Lexeme" << std::endl << std::endl;
+
+      Lexer analize; //construct lexer
+
+      while (!in_file.eof()) {
+        analize.lexer(in_file);
+
+				//print if lexeme doesnt = EOF
+				if (analize.getLexeme() != "EOF") {
+					out_file << std::left << std::setw(20) << analize.getToken() << std::setw(20) << analize.getLexeme() << std::endl;
+				}
+      }
+
+      in_file.close();
+			out_file << std::endl;
+      std::cout << "\noutput is in \"output.txt\"\n\n";
+    }
+  } while (filename != "stop");
+
+	out_file.close();
+  std::cout << "\nlexical analizer is done\n\n";
   return 0;
 }
